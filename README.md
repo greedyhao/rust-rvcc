4561bdbc8400ad0d05d6ffe3fa0d252fee643bac
greedyhao <hao_kr@163.com>
2022-09-12 09:21:08 +0800

    [0]编译出能返回指定数值的程序

---
+++ README.md
@@ -0,0 +1,3 @@
+本项目参考sunshaoce的[rvcc](https://github.com/sunshaoce/rvcc)，只是使用 rust 进行重写的工作，用于熟悉 rust 的语言与编译器原理
+
+可以观看[徒手写一个 RISC-V 编译器](https://www.bilibili.com/video/BV1gY4y1E7Ue/?spm_id_from=333.788&vd_source=f74cea619dafd4c3189d56d5392f33b1)，获取更好的体验
---
+++ src/main.rs
@@ -0,0 +1,64 @@
+use std::fs::File;
+use std::io::prelude::*;
+
+use clap::Parser;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    #[clap(short, long, value_parser)]
+    expression: String,
+}
+
+fn main_body(args: Args) {
+    let str1 = "  .global main\n";
+    let str2 = "main:\n";
+    let str3 = format!("  li a0, {}\n", args.expression);
+    let str4 = "  ret\n";
+
+    let str = str1.to_string() + str2 + &str3 + str4;
+
+    let mut file = File::create("tmp.s").unwrap();
+    write!(file, "{}", str).unwrap();
+}
+
+fn main() {
+    let args = Args::parse();
+    main_body(args)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    #[ignore]
+    fn test_run_clean() {
+        use std::process::Command;
+        Command::new("sh").arg("clean.sh").output().unwrap();
+    }
+
+    #[test]
+    fn test_no_args() {
+        use std::process::Command;
+        use std::str;
+
+        let test_integer = 1.to_string();
+        let args = Args {
+            expression: test_integer.clone(),
+        };
+        main_body(args);
+
+        let test = Command::new("sh")
+            .arg("test.sh")
+            .output()
+            .expect("failed to execute script");
+        println!("{:?} {:?} {:?}", test.status, test.stdout, test.stderr);
+
+        let stdout = [test.stdout[0]];
+        let stdout = str::from_utf8(&stdout).unwrap();
+        println!("{}", stdout);
+        assert_eq!(stdout, test_integer);
+    }
+}
