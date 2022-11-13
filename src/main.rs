103db7cb613b40b459f94ec617b2c57129f51627
greedyhao <hao_kr@163.com>
2022-11-14 06:43:29 +0800

    [5-1] 实现ast树

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,35 @@
+use core::slice::Iter;
 use std::fs::File;
 use std::io::prelude::*;
-use std::iter::{Enumerate, Peekable};
-use std::str::Chars;
+use std::iter::Peekable;
 
 use clap::Parser;
+use core::mem;
+use log::{info, trace};
+use std::fmt::Debug;
+
+mod binary_tree;
+use binary_tree::*;
+
+impl BinaryTreeIO<AstNode> for BinaryTree<AstNode> {
+    fn new_binary_without_right(
+        root: AstNode,
+        left: &mut BinaryTree<AstNode>,
+    ) -> BinaryTree<AstNode> {
+        let mut bt = BinaryTree::new(root);
+        bt.set_left(Some(Box::new(mem::take(left))));
+        return bt;
+    }
+}
+
+impl Default for BinaryTree<AstNode> {
+    fn default() -> Self {
+        BinaryTree::new(AstNode {
+            kind: NodeKind::Num,
+            value: String::new(),
+        })
+    }
+}
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
@@ -12,44 +38,294 @@ struct Args {
     expression: String,
 }
 
-fn iterchar2str(
-    current_char: char,
-    iter: &mut Peekable<Enumerate<Chars>>,
-) -> Result<String, (String, usize, char)> {
-    let mut num = current_char.to_string();
-    let mut check_operator = false;
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+enum TokenKind {
+    Other,
+    Punck,
+    Num,
+    // Eof,
+}
 
-    if !(current_char >= '0' && current_char <= '9') {
-        num = String::new(); // 当前字符非数字，清空
-        check_operator = true;
+#[derive(Debug, Clone)]
+struct Token {
+    kind: TokenKind,
+    value: String,
+    offset: usize,
+}
+
+#[derive(Debug, PartialEq)]
+enum NodeKind {
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Num,
+}
+
+#[derive(Debug)]
+pub struct AstNode {
+    kind: NodeKind,
+    value: String,
+}
+
+impl Default for AstNode {
+    fn default() -> Self {
+        AstNode {
+            kind: NodeKind::Num,
+            value: String::new(),
+        }
+    }
+}
+
+fn tokenize(strings: &str) -> Vec<Token> {
+    let count = strings.len() - 1;
+    let mut iter = strings.chars().enumerate().peekable();
+
+    let mut tokens: Vec<Token> = Vec::new();
+    let mut token_kind;
+    let mut token_kind_old = TokenKind::Other;
+    let mut token_value = String::new();
+    let mut offset = 0;
+
+    while let Some((index, ch)) = iter.peek() {
+        trace!("tokenize index={} ch={}", index, ch);
+        match *ch {
+            '0'..='9' => token_kind = TokenKind::Num,
+            '+' | '-' | '*' | '/' | '(' | ')' => token_kind = TokenKind::Punck,
+            _ => token_kind = TokenKind::Other,
+        }
+
+        trace!(
+            "tokenize token_kind={:?} token_kind_old={:?}",
+            token_kind,
+            token_kind_old
+        );
+
+        if !token_value.is_empty() {
+            if token_kind_old != token_kind || token_kind != TokenKind::Num {
+                let token = Token {
+                    kind: token_kind_old,
+                    value: token_value.clone(),
+                    offset,
+                };
+                trace!("tokenize tokens.push:{:?}", token);
+                tokens.push(token);
+                token_value.clear();
+            }
+        }
+
+        if *ch > ' ' && *ch < 0xff as char {
+            token_value.push(*ch);
+        }
+
+        offset = *index;
+        token_kind_old = token_kind;
+        trace!("tokenize token_value:{:?}", token_value);
+
+        // 如果是最后一个
+        if *index == count {
+            let token = Token {
+                kind: token_kind_old,
+                value: token_value.clone(),
+                offset,
+            };
+            trace!("tokenize tokens.push:{:?}", token);
+            tokens.push(token);
+        }
+        iter.next();
     }
 
-    while let Some(&(index, next_ch)) = iter.peek() {
-        match next_ch {
-            '0'..='9' => {
-                num.push(iter.next().unwrap().1);
-                check_operator = false;
+    return tokens;
+}
+
+// expr = mul ("+" mul | "-" mul)*
+// mul = primary ("*" primary | "/" primary)*
+// primary = "(" expr ")" | num
+fn expr(tokens: &mut Peekable<Iter<Token>>) -> Result<Option<BinaryTree<AstNode>>, String> {
+    let mut node = match mul(tokens)? {
+        Some(node) => node,
+        None => return Ok(None),
+    };
+
+    loop {
+        let token = match tokens.peek() {
+            Some(t) => t.clone().clone(),
+            None => return Ok(Some(node)),
+        };
+        trace!("-- expr {:?}", token);
+
+        match token.value.as_str() {
+            "+" => {
+                tokens.next();
+                trace!("expr {:?} == {:?}", tokens, token);
+
+                let mut bt = BinaryTree::new_binary_without_right(
+                    AstNode {
+                        kind: NodeKind::Add,
+                        value: token.value,
+                    },
+                    &mut node,
+                );
+
+                let right = mul(tokens)?;
+                if right.is_some() {
+                    bt.set_right(Some(Box::new(mem::take(&mut right.unwrap()))));
+                }
+                node = bt;
+                continue;
             }
-            ' ' => {
-                iter.next().unwrap();
+            "-" => {
+                tokens.next();
+                trace!("expr {:?} == {:?}", tokens, token);
+
+                let mut bt = BinaryTree::new_binary_without_right(
+                    AstNode {
+                        kind: NodeKind::Sub,
+                        value: token.value,
+                    },
+                    &mut node,
+                );
+
+                let right = mul(tokens)?;
+                if right.is_some() {
+                    bt.set_right(Some(Box::new(mem::take(&mut right.unwrap()))));
+                }
+                node = bt;
+                continue;
             }
             _ => {
-                if check_operator {
-                    match next_ch {
-                        '+' | '-' | '*' | '/' => {
-                            return Err(("expect a number".to_string(), index, next_ch));
-                        }
-                        'a'..='z' | 'A'..='Z' => {
-                            return Err(("invalid token".to_string(), index, next_ch));
-                        }
-                        _ => {}
-                    }
-                }
-                break;
+                return Ok(Some(node));
             }
         }
     }
-    return Ok(num.trim_start().to_string());
+}
+
+fn mul(tokens: &mut Peekable<Iter<Token>>) -> Result<Option<BinaryTree<AstNode>>, String> {
+    let mut node = match primary(tokens)? {
+        Some(node) => node,
+        None => return Ok(None),
+    };
+
+    loop {
+        let token = match tokens.peek() {
+            Some(t) => t.clone().clone(),
+            None => return Ok(Some(node)),
+        };
+        trace!("-- mul {:?}", token);
+
+        match token.value.as_str() {
+            "*" => {
+                tokens.next();
+
+                let mut bt = BinaryTree::new_binary_without_right(
+                    AstNode {
+                        kind: NodeKind::Mul,
+                        value: token.value,
+                    },
+                    &mut node,
+                );
+
+                let right = primary(tokens)?;
+                if right.is_some() {
+                    bt.set_right(Some(Box::new(mem::take(&mut right.unwrap()))));
+                }
+                node = bt;
+                continue;
+            }
+            "/" => {
+                tokens.next();
+
+                let mut bt = BinaryTree::new_binary_without_right(
+                    AstNode {
+                        kind: NodeKind::Div,
+                        value: token.value,
+                    },
+                    &mut node,
+                );
+
+                let right = primary(tokens)?;
+                if right.is_some() {
+                    bt.set_right(Some(Box::new(mem::take(&mut right.unwrap()))));
+                }
+                node = bt;
+                continue;
+            }
+            _ => {
+                return Ok(Some(node));
+            }
+        }
+    }
+}
+
+fn primary(tokens: &mut Peekable<Iter<Token>>) -> Result<Option<BinaryTree<AstNode>>, String> {
+    let token = match tokens.next() {
+        Some(t) => t,
+        None => return Ok(None),
+    };
+
+    match token.kind {
+        TokenKind::Punck => {
+            trace!("primary Punck {:?} == {:?}", tokens, token);
+            match token.value.as_str() {
+                "(" => {
+                    // expr(tokens, asts);
+                    return Ok(None);
+                }
+                _ => return Err(format!("{: <1$}", "", token.offset) + &format!("^ expect a number")),
+            }
+        }
+        TokenKind::Num => {
+            trace!("primary Num {:?} == {:?}", tokens, token);
+            // 新建一个节点
+            return Ok(Some(BinaryTree::new(AstNode {
+                kind: NodeKind::Num,
+                value: token.value.clone(),
+            })));
+        }
+        TokenKind::Other => {
+            // 正常不会有这个元素
+            // error
+            return Err(format!("{: <1$}", "", token.offset) + &format!("^ invalid token"));
+        }
+    }
+}
+
+fn push(asm: &mut String, depth: &mut isize) {
+    asm.push_str("  addi sp, sp, -8\n");
+    asm.push_str("  sd a0, 0(sp)\n");
+    *depth += 1;
+}
+
+fn pop(asm: &mut String, depth: &mut isize, reg: &str) {
+    asm.push_str(&format!("  ld {}, 0(sp)\n", reg));
+    asm.push_str("  addi sp, sp, 8\n");
+    *depth -= 1;
+}
+
+fn get_expr(ast: Option<&BinaryTree<AstNode>>, asm: &mut String, depth: &mut isize) {
+    trace!("get_expr ast {:?}", ast);
+    let root = ast.as_ref().unwrap().get_root();
+    if root.kind == NodeKind::Num {
+        asm.push_str(&format!("  li a0, {}\n", root.value));
+        trace!("get_expr num {:?}", root.value);
+        return;
+    }
+
+    get_expr(ast.as_ref().unwrap().get_right(), asm, depth);
+    push(asm, depth);
+    get_expr(ast.as_ref().unwrap().get_left(), asm, depth);
+    pop(asm, depth, "a1");
+
+    trace!("get_expr {:?} {:?}", root.kind, root.value);
+    match root.kind {
+        NodeKind::Add => asm.push_str("  add a0, a0, a1\n"),
+        NodeKind::Sub => asm.push_str("  sub a0, a0, a1\n"),
+        NodeKind::Mul => asm.push_str("  mul a0, a0, a1\n"),
+        NodeKind::Div => asm.push_str("  div a0, a0, a1\n"),
+        _ => {
+            // error
+        }
+    }
 }
 
 fn main_body(args: Args, asm_name: &str) -> Result<i32, String> {
@@ -58,59 +334,17 @@ fn main_body(args: Args, asm_name: &str) -> Result<i32, String> {
     let mut str3 = String::new();
     let str4 = "  ret\n";
 
-    let mut str3_format = "";
-    let str3_num = "  li a0, ";
+    let tokens = tokenize(&args.expression);
+    info!("tokens={:?}", tokens);
+    let mut aiter = tokens.iter().peekable();
+    let ast = match expr(&mut aiter) {
+        Ok(t) => t,
+        Err(e) => return Err(args.expression + "\n" + &e),
+    };
+    info!("{:?}", ast);
 
-    // addi rd, rs1, imm 表示 rd = rs1 + imm
-    // addi中imm为有符号立即数，所以加法表示为 rd = rs1 + imm
-    let str3_plus = "  addi a0, a0, ";
-
-    // addi rd, rs1, imm 表示 rd = rs1 + imm
-    // addi中imm为有符号立即数，所以减法表示为 rd = rs1 + (-imm)
-    let str3_minus = "  addi a0, a0, -";
-
-    // 这里我们将算式分解为 num (op num) (op num)...的形式
-    let mut iter = args.expression.chars().enumerate().peekable();
-    while let Some((_, ch)) = iter.next() {
-        match ch {
-            // 为num则传入a0
-            '0'..='9' => {
-                str3_format = str3_num;
-            }
-
-            // 为+则读取下一个num做加法
-            '+' => {
-                str3_format = str3_plus;
-            }
-
-            // 为-则读取下一个num做减法
-            '-' => {
-                str3_format = str3_minus;
-            }
-
-            // 如果是空格就忽略掉
-            ' ' => {}
-
-            _ => {
-                return Err("undefined".to_string());
-            }
-        }
-
-        if ch != ' ' {
-            let res = iterchar2str(ch, &mut iter);
-            match res {
-                Ok(num) => str3 += &format!("{}{}\n", str3_format, num),
-                Err((code, index, _)) => {
-                    // 错误打印信息
-                    let mut info = String::new();
-                    info += &format!("{}\n", args.expression);
-                    info += &format!("{: <1$}", "", index);
-                    info += &format!("^ {}", code);
-                    return Err(info);
-                }
-            }
-        }
-    }
+    let mut depth = 0;
+    get_expr(ast.as_ref(), &mut str3, &mut depth);
 
     let str = str1.to_string() + str2 + &str3 + str4;
 
@@ -121,6 +355,7 @@ fn main_body(args: Args, asm_name: &str) -> Result<i32, String> {
 }
 
 fn main() {
+    env_logger::init();
     let args = Args::parse();
     main_body(args, "tmp.s").unwrap();
 }
@@ -136,9 +371,36 @@ mod test {
         Command::new("sh").arg("clean.sh").output().unwrap();
     }
 
+    #[test]
+    #[ignore]
+    fn test_bt() {
+        let mut bt1 = BinaryTree::new(AstNode {
+            kind: NodeKind::Num,
+            value: "1".to_string(),
+        });
+        bt1.set_left(Some(Box::new(BinaryTree::new(AstNode {
+            kind: NodeKind::Num,
+            value: "0".to_string(),
+        }))));
+        let bt2 = BinaryTree::new_binary_without_right(
+            AstNode {
+                kind: NodeKind::Add,
+                value: String::new(),
+            },
+            &mut bt1,
+        );
+
+        let left = bt2.get_left();
+        let lleft = left.unwrap().get_left();
+
+        println!("{:?}", left);
+        println!("{:?}", lleft);
+    }
+
     fn test_xxx_do(input: &str, asm_name: &str) -> Result<String, String> {
         use std::process::Command;
         use std::str;
+        let _ = env_logger::builder().is_test(true).try_init();
 
         let args = Args {
             expression: input.to_string(),
@@ -153,13 +415,8 @@ mod test {
             .expect("failed to execute script");
 
         let stdout = str::from_utf8(&test.stdout).unwrap();
-        // let stderr = str::from_utf8(&test.stderr).unwrap();
-        // println!(
-        //     "sh test.sh => {:?} {:?} {:?}",
-        //     test.status,
-        //     stdout,
-        //     stderr,
-        // );
+        let stderr = str::from_utf8(&test.stderr).unwrap();
+        info!("sh test.sh => {:?} {:?} {:?}", test.status, stdout, stderr,);
         let tmp = stdout.to_string();
         let result: Vec<&str> = tmp.split('\n').collect();
         let result = result[0];
@@ -209,13 +466,19 @@ mod test {
     fn test_004_0_space_characters() {
         let asm_name = "tmp_004_0";
         let input = "1+s";
-        assert_eq!(test_xxx_do(input, asm_name), Err("1+s\n  ^ invalid token".to_string()));
+        assert_eq!(
+            test_xxx_do(input, asm_name),
+            Err("1+s\n  ^ invalid token".to_string())
+        );
     }
 
     #[test]
     fn test_004_1_space_characters() {
         let asm_name = "tmp_004_1";
         let input = "1++1";
-        assert_eq!(test_xxx_do(input, asm_name), Err("1++1\n  ^ expect a number".to_string()));
+        assert_eq!(
+            test_xxx_do(input, asm_name),
+            Err("1++1\n  ^ expect a number".to_string())
+        );
     }
 }
